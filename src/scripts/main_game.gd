fcf408c37b9e7270ec82adcfa4f33cfb537f42fc
lyhottag13 <leonardo@promoevt.com>
2026-08-14 00:48:07 -0700

    Added state machine for player and snow

--- src/scripts/main_game.gd
+++ src/scripts/main_game.gd
@@ -1,12 +1,6 @@
 extends Node
 
-@onready var player: CharacterBody2D = $World/Player
-
-const MAX_STRENGTH := 80
-const STRENGTH_MULTIPLIER := 6
-
 var red_tween: Tween
-var is_charging := false
 
 var old_mouse_position: Vector2
 @onready var crosshair: Sprite2D = %Crosshair
@@ -14,7 +8,7 @@ var old_mouse_position: Vector2
 @onready var halo: Panel = $CanvasLayer/Halo
 
 @onready var level: Level = $World/Level
-
+@onready var player: Player = $World/Player
 
 func _input(event: InputEvent) -> void:
 	if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_FULLSCREEN:
@@ -22,39 +16,7 @@ func _input(event: InputEvent) -> void:
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		
-		if event.pressed and player.is_on_floor():
-			is_charging = true
-			player.set_physics_process(false)
-			old_mouse_position = get_viewport().get_mouse_position()
-			crosshair.show()
-			crosshair.position = old_mouse_position
-			red_tween = create_tween()
-			red_tween.tween_property(player, "modulate", Color.RED, 0.5)
-			red_tween.parallel().tween_property(player, "scale:y", 0.8, 0.5)
-			red_tween.parallel().tween_property(player, "scale:x", 1.2, 0.5)
-			
-			halo.show()
-			halo.size = Vector2(MAX_STRENGTH * 2, MAX_STRENGTH * 2)
-			halo.position = old_mouse_position - halo.size / 2
-		elif not event.pressed and player.is_on_floor() and is_charging:
-			SoundManager.play_sfx("jump")
-			crosshair.hide()
-			halo.hide()
-			red_tween.kill()
-			player.modulate = Color.WHITE
-			player.scale.y = 1
-			player.scale.x = 1
-			
-			var new_mouse_position := get_viewport().get_mouse_position()
-			var difference := old_mouse_position - new_mouse_position
-			if difference.length() > MAX_STRENGTH:
-				difference = difference.normalized() * MAX_STRENGTH
-			player.velocity = difference * STRENGTH_MULTIPLIER
-			player.set_physics_process(true)
-			is_charging = false
-	elif event.is_action_pressed("skip"):
+	if event.is_action_pressed("skip"):
 		if event is InputEventKey:
 			match event.keycode:
 				KEY_Z:
@@ -78,3 +40,16 @@ func _on_level_leave() -> void:
 	await get_tree().create_timer(1).timeout
 	if OS.has_feature("web"):
 		JavaScriptBridge.eval("window.location.href='https://lyhottag13.github.io/shop-'")
+
+
+func _on_player_charging_started(crosshair_position: Vector2) -> void:
+	crosshair.show()
+	crosshair.position = crosshair_position
+	halo.show()
+	halo.size = Vector2(200, 200)
+	halo.position = crosshair_position - halo.size / 2
+
+
+func _on_player_charging_stopped() -> void:
+	crosshair.hide()
+	halo.hide()
--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -1,46 +1,133 @@
+class_name Player
 extends CharacterBody2D
 
+signal charging_started(crosshair_position: Vector2)
+signal charging_stopped
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -200.0
 const AIR_SPEED = 0
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-var old_mouse_position: Vector2
-var current_direction: Vector2
+const MAX_STRENGTH := 80
+const STRENGTH_MULTIPLIER := 6
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+#var old_mouse_position: Vector2
+
+var red_tween: Tween
+
+enum STATE {
+	IDLE,
+	RUNNING,
+	CHARGING,
+	JUMPING,
+	FALLING,
+}
+
+var state: STATE = STATE.IDLE:
+	set(new_state):
 		
-		if velocity.y < 0:
-			animated_sprite_2d.play("jump_up")
-			animated_sprite_2d.scale.y = clamp(1 + abs(velocity.y * 0.002), 1, 1.2)
-			animated_sprite_2d.scale.x = clamp(1 - abs(velocity.y * 0.001), 0.8, 1)
-		elif velocity.y > 0:
-			animated_sprite_2d.play("jump_down")
-			animated_sprite_2d.scale = Vector2(1, 1)
-	
-	else:
-		if velocity.x < 0:
-			animated_sprite_2d.play("running")
-			animated_sprite_2d.flip_h = true
-		elif velocity.x > 0:
-			animated_sprite_2d.play("running")
-			animated_sprite_2d.flip_h = false
-		elif velocity.x == 0:
-			animated_sprite_2d.play("idling")
-	
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+		state = new_state
+		match new_state:
+			STATE.IDLE:
+				animated_sprite_2d.play("idling")
+			STATE.RUNNING:
+				animated_sprite_2d.play("running")
+			STATE.JUMPING:
+				animated_sprite_2d.play("jumping")
+			STATE.FALLING:
+				animated_sprite_2d.play("falling")
+			STATE.CHARGING:
+				old_mouse_position = get_viewport().get_mouse_position()
+				charging_started.emit(old_mouse_position)
+				animated_sprite_2d.play("charging")
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("real_jump") and state in [STATE.IDLE, STATE.RUNNING]:
+		
+		red_tween = create_tween()
+		red_tween.tween_property(self, "modulate", Color.RED, 0.5)
+		red_tween.parallel().tween_property(self, "scale:y", 0.8, 0.5)
+		red_tween.parallel().tween_property(self, "scale:x", 1.2, 0.5)
+		
+	elif event.is_action_released("real_jump") and state == STATE.CHARGING:
+		
+		SoundManager.play_sfx("jump")
+		red_tween.kill()
+		modulate = Color.WHITE
+		scale.y = 1
+		scale.x = 1
+		
+	elif event.is_action_pressed("jump") and state in [STATE.IDLE, STATE.RUNNING]:
+		state = STATE.JUMPING
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
+var old_mouse_position: Vector2
+
+func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("move_left", "move_right")
-	if direction and is_on_floor():
-		velocity.x = direction * SPEED
-	elif is_on_floor() and velocity.y == 0:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	match state:
+		STATE.IDLE:
+			if Input.is_action_just_pressed("jump"):
+				state = STATE.JUMPING
+				velocity.y = JUMP_VELOCITY
+			elif not is_on_floor():
+				state = STATE.FALLING
+			elif direction:
+				state = STATE.RUNNING
+				velocity.x = direction * SPEED
+			elif velocity.x != 0:
+				velocity.x = move_toward(velocity.x, 0, SPEED)
+			elif Input.is_action_pressed("real_jump"):
+				state = STATE.CHARGING
+		STATE.RUNNING:
+			if not is_on_floor():
+				state = STATE.FALLING
+			elif Input.is_action_pressed("real_jump"):
+				state = STATE.CHARGING
+			elif Input.is_action_pressed("jump"):
+				state = STATE.JUMPING
+				velocity.y = JUMP_VELOCITY
+			elif direction:
+				velocity.x = direction * SPEED
+			else:
+				state = STATE.IDLE
+		STATE.CHARGING:
+			velocity.x = 0
+			
+			var new_mouse_position = get_viewport().get_mouse_position()
+			
+			if new_mouse_position.x > old_mouse_position.x:
+				animated_sprite_2d.flip_h = true
+			elif new_mouse_position.x < old_mouse_position.x:
+				animated_sprite_2d.flip_h = false
+			
+			if not Input.is_action_pressed("real_jump"):
+				state = STATE.JUMPING
+				
+				charging_stopped.emit()
+				
+				var difference: Vector2 = old_mouse_position - new_mouse_position
+				
+				if difference.length() > MAX_STRENGTH:
+					difference = difference.normalized() * MAX_STRENGTH
+				
+				velocity = difference * STRENGTH_MULTIPLIER
+		STATE.JUMPING:
+			velocity += delta * get_gravity()
+			if velocity.y > 0:
+				state = STATE.FALLING
+		STATE.FALLING:
+			velocity += delta * get_gravity()
+			if is_on_floor():
+				state = STATE.IDLE
+	
+	if velocity.x < 0:
+		animated_sprite_2d.flip_h = true
+	elif velocity.x > 0:
+		animated_sprite_2d.flip_h = false
+	
+	print_debug(STATE.find_key(state))
 	
 	move_and_slide()
