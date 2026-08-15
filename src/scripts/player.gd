3c096869e62b82071b4a7cb5eb2becc1faafda69
lyhottag13 <leonardo@promoevt.com>
2026-08-14 18:53:17 -0700

    Added regular states and changed wood sprite

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -21,26 +21,33 @@ enum STATE {
 	RUNNING,
 	CHARGING,
 	JUMPING,
+	REGULAR_JUMPING,
 	FALLING,
+	REGULAR_FALLING,
 }
 
 var state: STATE = STATE.IDLE:
 	set(new_state):
+		var old_state: STATE = state
+		match old_state:
+			STATE.JUMPING:
+				animated_sprite_2d.scale = Vector2(1, 1)
 		
-		state = new_state
 		match new_state:
 			STATE.IDLE:
 				animated_sprite_2d.play("idling")
 			STATE.RUNNING:
 				animated_sprite_2d.play("running")
-			STATE.JUMPING:
+			STATE.JUMPING, STATE.REGULAR_JUMPING:
 				animated_sprite_2d.play("jumping")
-			STATE.FALLING:
+			STATE.FALLING, STATE.REGULAR_FALLING:
 				animated_sprite_2d.play("falling")
 			STATE.CHARGING:
 				old_mouse_position = get_viewport().get_mouse_position()
 				charging_started.emit(old_mouse_position)
 				animated_sprite_2d.play("charging")
+		state = new_state
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("real_jump") and state in [STATE.IDLE, STATE.RUNNING]:
@@ -59,7 +66,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		scale.x = 1
 		
 	elif event.is_action_pressed("jump") and state in [STATE.IDLE, STATE.RUNNING]:
-		state = STATE.JUMPING
+		state = STATE.REGULAR_JUMPING
 		velocity.y = JUMP_VELOCITY
 
 var old_mouse_position: Vector2
@@ -79,12 +86,13 @@ func _physics_process(delta: float) -> void:
 				velocity.x = direction * SPEED
 			elif velocity.x != 0:
 				velocity.x = move_toward(velocity.x, 0, SPEED)
-			elif Input.is_action_pressed("real_jump"):
+			elif Input.is_action_just_pressed("real_jump"):
 				state = STATE.CHARGING
+		
 		STATE.RUNNING:
 			if not is_on_floor():
 				state = STATE.FALLING
-			elif Input.is_action_pressed("real_jump"):
+			elif Input.is_action_just_pressed("real_jump"):
 				state = STATE.CHARGING
 			elif Input.is_action_pressed("jump"):
 				state = STATE.JUMPING
@@ -93,6 +101,7 @@ func _physics_process(delta: float) -> void:
 				velocity.x = direction * SPEED
 			else:
 				state = STATE.IDLE
+		
 		STATE.CHARGING:
 			velocity.x = 0
 			
@@ -103,7 +112,7 @@ func _physics_process(delta: float) -> void:
 			elif new_mouse_position.x < old_mouse_position.x:
 				animated_sprite_2d.flip_h = false
 			
-			if not Input.is_action_pressed("real_jump"):
+			if Input.is_action_just_released("real_jump"):
 				state = STATE.JUMPING
 				
 				charging_stopped.emit()
@@ -114,14 +123,41 @@ func _physics_process(delta: float) -> void:
 					difference = difference.normalized() * MAX_STRENGTH
 				
 				velocity = difference * STRENGTH_MULTIPLIER
+		
 		STATE.JUMPING:
 			velocity += delta * get_gravity()
 			if velocity.y > 0:
 				state = STATE.FALLING
+			else:
+				animated_sprite_2d.scale.y = clamp(1 + abs(velocity.y * 0.002), 1, 1.2)
+				animated_sprite_2d.scale.x = clamp(1 - abs(velocity.y * 0.001), 0.8, 1)
+		
+		STATE.REGULAR_JUMPING:
+			velocity += delta * get_gravity()
+			if velocity.y > 0:
+				state = STATE.REGULAR_FALLING
+			else:
+				animated_sprite_2d.scale.y = clamp(1 + abs(velocity.y * 0.002), 1, 1.2)
+				animated_sprite_2d.scale.x = clamp(1 - abs(velocity.y * 0.001), 0.8, 1)
+			
+			if direction:
+				velocity.x = SPEED * direction
+			else:
+				velocity.x = move_toward(velocity.x, 0, SPEED)
+		
 		STATE.FALLING:
 			velocity += delta * get_gravity()
 			if is_on_floor():
 				state = STATE.IDLE
+		
+		STATE.REGULAR_FALLING:
+			velocity += delta * get_gravity()
+			if is_on_floor():
+				state = STATE.IDLE
+			if direction:
+				velocity.x = SPEED * direction
+			else:
+				velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	if velocity.x < 0:
 		animated_sprite_2d.flip_h = true
